18a18555b9cea99e943e3ffe6f398bb0a5186fb2
Charles Chavis <cchavis3@gmail.com>
2020-09-09 07:38:25 -0400

    set up sticky nav and banner

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
 	return (
 		// BEM Convention for styling
 		<div className="app">
-			<h1>Building Apps to Save Our Lives. First up, Amaxon Store!</h1>
 			<Header />
-			{/* Home */}
+			<Home />
 		</div>
 	);
 }
---
+++ amazon-clone/src/Home.js
@@ -0,0 +1,37 @@
+// rfce
+import React from 'react';
+import "./Home.css"
+import "./Product"
+import Product from './Product';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img 
+                    className="home__image" 
+                    src="https://images-na.ssl-images-amazon.com/images/G/01/Audible/en_US/images/creative/amazon/Minerva_Gateway_Hero_Desktop_070820_PremiumPlus_1x._CB410855810_.jpg" 
+                alt=""
+                />
+
+                <div className="home__row">
+                    <Product />
+                </div>
+
+                <div className="home__row">
+                    {/* Product */}
+                    {/* Product */}
+                    {/* Product */}
+                </div>
+
+                <div className="home__row">
+                    {/* Product */}
+                    {/* Product */}
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
