67716fc1d9747a3e60fee6f2d5627209a6975ac5
Charles Chavis <cchavis3@gmail.com>
2020-09-08 22:14:40 -0400

    set up header

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+	return (
+		// BEM Convention for styling
+		<div className="app">
+			<h1>Building Apps to Save Our Lives. First up, Amaxon Store!</h1>
+			<Header />
+			{/* Home */}
+		</div>
+	);
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDxUtGj5cEo873y0oubgBdWdt55ZZdIz4w",
+    authDomain: "amaxon-65b38.firebaseapp.com",
+    databaseURL: "https://amaxon-65b38.firebaseio.com",
+    projectId: "amaxon-65b38",
+    storageBucket: "amaxon-65b38.appspot.com",
+    messagingSenderId: "28719165333",
+    appId: "1:28719165333:web:d3e977231de6c112b5109c",
+    measurementId: "G-Q6G3QME3H7"
+  };
