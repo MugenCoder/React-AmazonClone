343a743767b75285536ff45301feb0f25c6fdaea
Charles Chavis <cchavis3@gmail.com>
2020-09-11 09:03:14 -0400

    implemented checkout subtotal; initialstate reducer created without render

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,14 +2,25 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";	
+import Checkout from "./Checkout.js";
 
 function App() {
 	return (
-		// BEM Convention for styling
-		<div className="app">
-			<Header />
-			<Home />
-		</div>
+		<Router>
+			<div className="app">
+				<Header /> {/* Persisting Header; outside of Switch Route */}
+
+				<Switch>
+					<Route path="/checkout">
+						<Checkout />
+					</Route>
+					<Route path="/"> {/* Default [Home Page] Route must be at bottom */}
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
---
+++ amazon-clone/src/reducer.js
@@ -0,0 +1,22 @@
+// implement an empty arr as our [default] initial cart state 
+export const initialState = {
+	basket: [],
+}
+
+// context api & redux aren't the same but use the same patter: A global store for your app with patterns that dispatch actions into the store 
+// use reducer to listen for events & dispatch actions and compile them accodingly (ex. add/remove an item from cart)
+const reducer = (state, action) => {
+
+	switch(action.type) {
+		case 'ADD_TO_BASKET':
+			return {
+				...state,
+				basket: [...state.basket, action.item],
+			};
+		
+		default:
+			return state;	
+	}
+};
+
+export default reducer;
