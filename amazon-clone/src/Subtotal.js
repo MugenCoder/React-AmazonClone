c3cbc2e35b22bcc13b6df412f843defc1d45e8c9
Charles Chavis <cchavis3@gmail.com>
2020-09-11 21:15:15 -0400

    cart subtotal functionality complete

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -2,7 +2,8 @@ import React from 'react'
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format"
 import { useStateValue } from "./StateProvider";
-import Product from "./Product.js";
+import Product from "./Product" 
+import { getBasketTotal } from './reducer';
 
 function Subtotal() {
 		const [{ basket }, dispatch] = useStateValue();
@@ -13,7 +14,7 @@ function Subtotal() {
 				<>
 					<p>
 						Subtotal ({basket?.length} items):
-						<strong>{price}</strong>
+						<strong>{value}</strong>
 					</p>
 					<small className="subtotal__gift">
 						<input type="checkbox" /> This order contains a gift
@@ -21,7 +22,7 @@ function Subtotal() {
 				</>
 			)}	
 			decimalScale={2}
-			value={0}	// default price
+			value={getBasketTotal(basket)}	// default value is set to 0 until items add to cart
 			displayType={"text"}
 			thousandSeparator={true}
 			prefix={"$"}
--- amazon-clone/src/reducer.js
+++ amazon-clone/src/reducer.js
@@ -3,6 +3,12 @@ export const initialState = {
 	basket: [],
 }
 
+// create a Selector with reduce() to total up basket item prices to return as the Subtotal value
+export const getBasketTotal = (basket) => 
+	// use optional chaining (?) on basket for handling "basket property undefined" console errors
+	basket?.reduce((amount, item) => item.price + amount, 0);
+
+
 // context api & redux aren't the same but use the same patter: A global store for your app with patterns that dispatch actions into the store 
 // use reducer to listen for events & dispatch actions and compile them accodingly (ex. add/remove an item from cart)
 const reducer = (state, action) => {
